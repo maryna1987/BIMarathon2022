05ff12ef719feb2eef146ebbf7b99a661c856931
maryna1987 <manulikm@gmail.com>
2022-04-03 10:48:39 -0700

    Add files via upload

---
+++ Data Model/University_Script.sql
@@ -0,0 +1,19 @@
+create database BI2022;
+use BI2022;
+
+CREATE TABLE temp (
+    university VARCHAR(500),
+    year INT,
+    rank_display VARCHAR(20),
+    score FLOAT,
+    link VARCHAR(255),
+    country VARCHAR(100),
+    city VARCHAR(100),
+    region VARCHAR(100),
+    type VARCHAR(500),
+    research_output VARCHAR(100),
+    student_faculty_ratio INT,
+    international_students FLOAT,
+    size VARCHAR(20),
+    faculty_count FLOAT
+);
